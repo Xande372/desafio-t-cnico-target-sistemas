0ebcf5ae34a283d1a9fea214af6f7513d0e3e3e9
Alexandre Direito <xandedsj@gmail.com>
2024-09-03 16:34:42 -0300

    desafio 3 feito!

---
+++ Questoes/src/Soma.java
@@ -0,0 +1,14 @@
+public class Soma {
+
+    public static void main(String[] args) {
+    
+        int INDICE = 12;
+        int K = 1;
+
+        // Soma da progressão aritmética de 2 a INDICE
+        int SOMA = ((INDICE * (INDICE + 1)) / 2) - ((K * (K + 1)) / 2);
+
+        // o valor da soma é 77.
+        System.out.println(SOMA);
+    }
+}
